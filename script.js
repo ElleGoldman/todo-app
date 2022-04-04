433ccad5df1bca39fe21a0fb42d4eb329cc179c2
Goldman Elena <goldmanelena@Goldmans-MacBook-Pro.local>
2022-04-04 17:04:43 +0300

    main todo item actions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const todos = [
+let todos = [
     {
         title: 'Task 1',
         isCompleted: false,
@@ -27,9 +27,9 @@ const todos = [
 
 // Markup
 const list = document.getElementById('todosList');
-function getUncompletedItem(title) {
+function getUncompletedItem(title, index) {
     return `
-        <li>
+        <li data-index="${index}">
             <span class="task-status"></span>
             <p class="task-title">${title}</p>
             <div class="task-actions">
@@ -40,9 +40,9 @@ function getUncompletedItem(title) {
         </li>
     `
 }
-function getCompletedItem(title) {
+function getCompletedItem(title, index) {
     return `
-        <li class="is-completed">
+        <li class="is-completed" data-index="${index}">
             <span class="task-status">
                 <i class="fa-solid fa-check"></i>
             </span>
@@ -55,29 +55,30 @@ function getCompletedItem(title) {
 }
 function generateTodos(data) {
     list.innerHTML = '';
-    data.forEach((item) => {
+    data.forEach((item, index) => {
         list.innerHTML += item.isCompleted
-            ? getCompletedItem(item.title)
-            : getUncompletedItem(item.title);
+            ? getCompletedItem(item.title, index)
+            : getUncompletedItem(item.title, index);
     })
 }
 generateTodos(todos);
 
 // Search
-const searchForm = document.getElementById('searchForm')
-const searchInput = document.getElementById('search')
+const searchForm = document.getElementById('searchForm');
+const searchInput = document.getElementById('search');
 
 function searchTodo(e) {
     e.preventDefault();
     const filteredTodos = todos.filter((item) => {
-        return item.title.toLowerCase().includes(searchInput.value)
+        return item.title.toLowerCase().includes(searchInput.value);
     })
     generateTodos(filteredTodos);
 }
-searchForm.addEventListener('submit', searchTodo)
-searchInput.addEventListener('input', searchTodo)
+searchForm.addEventListener('submit', searchTodo);
+searchInput.addEventListener('input', searchTodo);
+
 // Add
-const addButton = document.getElementById('add')
+const addButton = document.getElementById('add');
 function addTodo() {
     const title = window.prompt('Enter Title')
     todos.unshift({
@@ -86,4 +87,44 @@ function addTodo() {
     })
     generateTodos(todos);
 }
-addButton.addEventListener('click', addTodo)
+addButton.addEventListener('click', addTodo);
+
+// -----------
+
+function toggleTodoStatus(index) {
+    todos[index].isCompleted = !todos[index].isCompleted
+    generateTodos(todos);
+}
+
+function editTodo(index) {
+    const newTitle = window.prompt('Update Title', todos[index].title)
+    todos[index].title = newTitle;
+    console.log(todos);
+    generateTodos(todos);
+}
+
+function copyTodo(index) {
+    todos.splice(index, 0, {...todos[index]});
+    generateTodos(todos);
+}
+
+function deleteTodo(index) {
+    todos = todos.filter((todo, i) => i !== index)
+    generateTodos(todos);
+}
+
+list.addEventListener("click",function(e) {
+    const index = +e.target.closest('li').getAttribute('data-index');
+    
+    const checkClicked = e.target.matches("span.task-status") || e.target.matches("i.fa-check");
+    if (checkClicked) toggleTodoStatus(index);
+
+    const editButtonClicked = e.target.matches("button.is-edit") || e.target.matches("i.fa-pen-to-square");
+    if (editButtonClicked) editTodo(index);
+    
+    const copyButtonClicked = e.target.matches("button.is-copy") || e.target.matches("i.fa-copy");
+    if (copyButtonClicked) copyTodo(index);
+
+    const deleteButtonClicked = e.target.matches("button.is-danger") || e.target.matches("i.fa-trash");
+    if (deleteButtonClicked) deleteTodo(index);
+});
